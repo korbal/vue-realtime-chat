a2e25b46cf1b39fcef599daafdbd26d196ab5610
Balint <korosibalint@yahoo.co.uk>
2019-12-24 15:16:41 +0100

    l63, route guard implemented to protect /chat

--- vuechat/src/router/index.js
+++ vuechat/src/router/index.js
@@ -17,7 +17,15 @@ export default new Router({
       name: "Chat",
       component: Chat,
       // to pass the username from the welcome screen to /chat, we need props enabled. also need t oaccept them in Chat.vue as props. the actual passing happens in Welcome.vue enterChat() and surprisingly not with props but with params ($router)
-      props: true
+      props: true,
+      //route guard, so if user doesn't enter name, they can't access the chat component
+      beforeEnter: (to, from, next) => {
+        if (to.params.name) {
+          next();
+        } else {
+          next({ name: "Welcome" });
+        }
+      }
     }
   ]
 });
