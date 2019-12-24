6d1d93878173628e7563dabf8d6b976f8fd83e7c
Balint <korosibalint@yahoo.co.uk>
2019-12-24 15:10:04 +0100

    l62, passing username to chat done

--- vuechat/src/components/Welcome.vue
+++ vuechat/src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent="enterChat">
           <label for="name">Enter your name:</label>
           <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">{{feedback}}</p>
           <button class="btn teal">Enter Chat</button>
         </form>
       </div>
@@ -18,12 +19,20 @@ export default {
   name: "Welcome",
   data() {
     return {
-      name: null
+      name: null,
+      feedback: null
     };
   },
   methods: {
     enterChat() {
-      console.log(this.name);
+      if (this.name) {
+        //params passing through the entered name to /chat
+        this.$router.push({ name: "Chat", params: { name: this.name } });
+
+        // if they didn't enter a name, give text feedback abouve submit button
+      } else {
+        this.feedback = "You must enter a name to join";
+      }
     }
   }
 };
--- vuechat/src/router/index.js
+++ vuechat/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Welcome from "@/components/Welcome";
+import Chat from "@/components/Chat";
 
 Vue.use(Router);
 
@@ -10,6 +11,13 @@ export default new Router({
       path: "/",
       name: "Welcome",
       component: Welcome
+    },
+    {
+      path: "/chat",
+      name: "Chat",
+      component: Chat,
+      // to pass the username from the welcome screen to /chat, we need props enabled. also need t oaccept them in Chat.vue as props. the actual passing happens in Welcome.vue enterChat() and surprisingly not with props but with params ($router)
+      props: true
     }
   ]
 });
