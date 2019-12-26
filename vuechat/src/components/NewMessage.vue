a98e6d5cf3a89c8c00c530d6eab1be2584f0ee3c
Balint <korosibalint@yahoo.co.uk>
2019-12-26 14:12:19 +0100

    firebase not loading for some reason

--- vuechat/src/components/NewMessage.vue
+++ vuechat/src/components/NewMessage.vue
@@ -3,22 +3,29 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New Message (enter to add):</label>
       <input type="text" name="new-message" v-model="newMessage" />
+      <p class="red-text" v-if="feedback">{{feedback}}</p>
     </form>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init.js";
+
 export default {
   name: "NewMessage",
   props: ["name"],
   data() {
     return {
-      newMessage: null
+      newMessage: null,
+      feedback: null
     };
   },
   methods: {
     addMessage() {
-      console.log(this.newMessage, this.name, Date.now());
+      if (this.newMessage) {
+      } else {
+        this.feedback = "Enter message!";
+      }
     }
   }
 };
